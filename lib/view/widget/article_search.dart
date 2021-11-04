df92fc7997625a48a1d8d0a7484c48f98abc374b
Jannes <jannes.luca@hotmail.com>
2021-11-04 14:34:44 +0100

    searchHistory is now fully functional and displays on opening too

--- lib/view/widget/article_search.dart
+++ lib/view/widget/article_search.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 
 class ArticleSearch extends SearchDelegate<String> {
 
-  final SharedPreferences prefs = SharedPreferences.getInstance() as SharedPreferences;
-
   final articles = [ //TODO: Just sample articles. Needs actual data
     'iPhone',
     'Jeans',
@@ -14,55 +12,66 @@ class ArticleSearch extends SearchDelegate<String> {
     'charger',
   ];
 
-  List<String> recentArticles = [''];
+  List<String> recentArticles = [];
+
+  ArticleSearch () {
+    updateRecent();
+  }
 
   @override
-  List<Widget>? buildActions(BuildContext context) => [
-    IconButton(
-      icon: const Icon(Icons.clear),
-      onPressed: () {
-        if (query.isEmpty) {
-          close(context, ''); // Not sure if empty string is okay here, but it works
-        } else {
-          query = '';
-        }
-      },
-    )
-  ];
-
-  @override
-  Widget? buildLeading(BuildContext context) => IconButton(
-    icon: const Icon(Icons.arrow_back),
-    onPressed: () => close(context, ''), // Not sure if empty string is okay here, but it works
-  );
-
-  @override
-  Widget buildResults(BuildContext context) {
-    recentArticles.insert(0, query);
-    prefs.setStringList('recentArticles', recentArticles);
-
-    return Center( //TODO: just a placeholder site yet. Have to call a rearrangement of the article stream
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Icon(Icons.shop, size: 120),
-          const SizedBox(height: 48),
-          Text(
-            query,
-            style: const TextStyle(
-              color: Colors.black,
-              fontSize: 64,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ],
-      ),
-    );
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          if (query.isEmpty) {
+            close(context,
+                ''); // Not sure if empty string is okay here, but it works
+          } else {
+            query = '';
+          }
+        },
+      )
+    ];
   }
 
     @override
+    Widget? buildLeading(BuildContext context) {
+      return IconButton(
+        icon: const Icon(Icons.arrow_back),
+        onPressed: () =>
+            close(context,
+                ''), // Not sure if empty string is okay here, but it works
+      );
+    }
+
+    @override
+    Widget buildResults(BuildContext context) {
+      storeToRecent(query);
+      updateRecent();
+
+      return Center( //TODO: just a placeholder site yet. Have to call a rearrangement of the article stream
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(Icons.shop, size: 120),
+            const SizedBox(height: 48),
+            Text(
+              query,
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 64,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    @override
     Widget buildSuggestions(BuildContext context) {
-      recentArticles = prefs.getStringList('recentArticles') ?? [];
+      updateRecent();
       final suggestions = query.isEmpty ? recentArticles : articles.where((article) {
         final articleLower = article.toLowerCase();
         final queryLower = query.toLowerCase();
@@ -111,9 +120,20 @@ class ArticleSearch extends SearchDelegate<String> {
       },
     );
 
-  void storeToRecent(String article) {
-    recentArticles.insert(0, query);
-  }
+    void storeToRecent(String query) async {
+      if(query == '' || query == ' ') return;
+      recentArticles.insert(0, query);
+
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.setStringList('recentArticles', recentArticles);
+
+    }
+
+    void updateRecent() async {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      recentArticles = prefs.getStringList('recentArticles') ?? [];
+    }
 }
+
     
 
