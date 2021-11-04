0aa2e01d0909ca408d69b0ceb4773968c75588d0
Jannes <jannes.luca@hotmail.com>
2021-11-04 13:17:04 +0100

    included sharedPreferences and adapted searchBar.

--- lib/view/widget/article_search.dart
+++ lib/view/widget/article_search.dart
@@ -1,6 +1,10 @@
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 
 class ArticleSearch extends SearchDelegate<String> {
+
+  final SharedPreferences prefs = SharedPreferences.getInstance() as SharedPreferences;
+
   final articles = [ //TODO: Just sample articles. Needs actual data
     'iPhone',
     'Jeans',
@@ -10,11 +14,7 @@ class ArticleSearch extends SearchDelegate<String> {
     'charger',
   ];
 
-  final recentArticles = [ //TODO: Just sample articles. Needs actual data
-    'macBook',
-    'charger',
-    'Jeans',
-  ];
+  List<String> recentArticles = [''];
 
   @override
   List<Widget>? buildActions(BuildContext context) => [
@@ -37,73 +37,83 @@ class ArticleSearch extends SearchDelegate<String> {
   );
 
   @override
-  Widget buildResults(BuildContext context) => Center( //TODO: just a placeholder site yet. Have to call a rearrangement of the article stream
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Icon(Icons.shop, size: 120),
-        const SizedBox(height: 48),
-        Text(
-          query,
-          style: const TextStyle(
-            color: Colors.black,
-            fontSize: 64,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ],
-    ),
-  );
+  Widget buildResults(BuildContext context) {
+    recentArticles.insert(0, query);
+    prefs.setStringList('recentArticles', recentArticles);
 
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    final suggestions = query.isEmpty ? recentArticles : articles.where((article) {
-      final articleLower = article.toLowerCase();
-      final queryLower = query.toLowerCase();
-
-      return articleLower.startsWith(queryLower);
-    }).toList();
-
-    return buildSuggestionsSuccess(suggestions);
-  }
-
-  Widget buildSuggestionsSuccess(List<String> suggestions) => ListView.builder(
-    itemCount: suggestions.length,
-    itemBuilder: (context, index) {
-      final suggestion = suggestions[index];
-      final queryText = suggestion.substring(0, query.length);
-      final remainingText = suggestion.substring(query.length);
-
-      return ListTile(
-        onTap: () {
-          query = suggestion;
-
-          showResults(context);
-        },
-        leading: const Icon(Icons.shop),
-        // below: highlighted text for matching characters
-        title: RichText(
-          text: TextSpan(
-            text: queryText,
+    return Center( //TODO: just a placeholder site yet. Have to call a rearrangement of the article stream
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(Icons.shop, size: 120),
+          const SizedBox(height: 48),
+          Text(
+            query,
             style: const TextStyle(
               color: Colors.black,
+              fontSize: 64,
               fontWeight: FontWeight.bold,
-              fontSize: 18,
             ),
-            children: [
-              TextSpan(
-                text: remainingText,
-                style: const TextStyle(
-                  color: Colors.grey,
-                  fontSize: 18,
-                ),
-              ),
-            ],
           ),
-        ),
-      );
-    },
-  );
+        ],
+      ),
+    );
+  }
+
+    @override
+    Widget buildSuggestions(BuildContext context) {
+      recentArticles = prefs.getStringList('recentArticles') ?? [];
+      final suggestions = query.isEmpty ? recentArticles : articles.where((article) {
+        final articleLower = article.toLowerCase();
+        final queryLower = query.toLowerCase();
+
+        return articleLower.startsWith(queryLower);
+      }).toList();
+
+      return buildSuggestionsSuccess(suggestions);
+    }
+
+    Widget buildSuggestionsSuccess(List<dynamic> suggestions) => ListView.builder(
+      itemCount: suggestions.length,
+      itemBuilder: (context, index) {
+        final suggestion = suggestions[index];
+        final queryText = suggestion.substring(0, query.length);
+        final remainingText = suggestion.substring(query.length);
+
+        return ListTile(
+          onTap: () {
+            query = suggestion;
+
+            showResults(context);
+          },
+          leading: const Icon(Icons.shop),
+          // below: highlighted text for matching characters
+          title: RichText(
+            text: TextSpan(
+              text: queryText,
+              style: const TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+              children: [
+                TextSpan(
+                  text: remainingText,
+                  style: const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+
+  void storeToRecent(String article) {
+    recentArticles.insert(0, query);
+  }
+}
     
 
-}
