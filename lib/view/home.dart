48299e58488349c79b83d6c407c16cf28329ecc5
maginkaaa <57997851+maginkaaa@users.noreply.github.com>
2021-12-13 12:29:57 +0100

    update after merge with main #139

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -13,15 +13,13 @@ import 'package:penny_pincher/view/widget/extended_view.dart';
 import 'package:provider/provider.dart';
 import 'package:timezone/data/latest.dart' as tz;
 
-
 StreamController<bool> streamController = StreamController<bool>.broadcast();
 
 class HomePage extends StatefulWidget {
-
   late final Stream<bool> stream;
   late final StreamController updateStream;
-
-  HomePage(this.stream, this.updateStream);
+  final dynamic callback;
+  HomePage(this.stream, this.updateStream, this.callback);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -48,8 +46,7 @@ class _HomePageState extends State<HomePage> {
         if (_scrollController.offset <
             _scrollController.position.maxScrollExtent) {
           isScrolling = true;
-        }
-        else {
+        } else {
           isScrolling = false;
         }
       });
@@ -71,6 +68,7 @@ class _HomePageState extends State<HomePage> {
         _isLoading = false;
       });
     }
+    widget.callback.loadingFinished();
     if (count < _product.length) {
       _products.insert(count, _product[count]);
       count++;
@@ -97,28 +95,25 @@ class _HomePageState extends State<HomePage> {
     streamController.add(true);
   }
 
-
   void initListOfIDs() {
-    _jsonFunctions.getListOfProdCatIDs()
-        .then((value) => timerFunction(value));
+    _jsonFunctions.getListOfProdCatIDs().then((value) => timerFunction(value));
   }
 
-    timerFunction(List<int> ids) {
+  timerFunction(List<int> ids) {
     // every 2 seconds get a random category id, call the api with it, load the product and animate it
-      _timer = Timer.periodic(const Duration(seconds: 2), (Timer timer) {
-        index = _jsonFunctions.getRandomInt();
-        randomCategory = ids[index];
-        getProducts(randomCategory);
-        if (_scrollController.hasClients && !isScrolling) {
-          _scrollController.animateTo(
-            _scrollController.position.maxScrollExtent,
-            curve: Curves.easeOut,
-            duration: const Duration(milliseconds: 300),
-          );
-        }
-      });
-    }
-
+    _timer = Timer.periodic(const Duration(seconds: 2), (Timer timer) {
+      index = _jsonFunctions.getRandomInt();
+      randomCategory = ids[index];
+      getProducts(randomCategory);
+      if (_scrollController.hasClients && !isScrolling) {
+        _scrollController.animateTo(
+          _scrollController.position.maxScrollExtent,
+          curve: Curves.easeOut,
+          duration: const Duration(milliseconds: 300),
+        );
+      }
+    });
+  }
 
   @override
   void initState() {
@@ -175,8 +170,7 @@ class _HomePageState extends State<HomePage> {
                 SizedBox(width: 10),
                 Padding(
                   padding: EdgeInsets.only(top: 3),
-                  child:
-                  Text(
+                  child: Text(
                     'Penny Pincher',
                     style: TextStyle(
                       // Shaddow is used to get Distance to the underline -> TextColor itself is transparent
@@ -190,70 +184,71 @@ class _HomePageState extends State<HomePage> {
                       letterSpacing: 3,
                       color: Colors.transparent,
                       fontWeight: FontWeight.w900,
-                      decoration:
-                      TextDecoration.underline,
+                      decoration: TextDecoration.underline,
                       decorationColor: ThemeChanger.highlightedColor,
                       decorationThickness: 4,
                     ),
-                  ), ),
+                  ),
+                ),
               ],
             ),
             actions: [
               IconButton(
                 icon: Icon(Icons.search),
                 onPressed: () {
-                  final results =
-                  showSearch(context: context, delegate: ArticleSearch(true, this, streamController));
+                  final results = showSearch(
+                      context: context,
+                      delegate: ArticleSearch(true, this, streamController));
                 },
               )
-            ]
-        ),
-
+            ]),
         body: _isLoading
             ? Center(child: CircularProgressIndicator())
             : Align(
-            alignment: Alignment.topCenter,
-            child: NotificationListener<ScrollNotification>(
-              onNotification: (scrollNotification) {
-                if (scrollNotification is ScrollUpdateNotification) {
-                  _onUpdateScroll();
-                }
-                return true;
-              },
-              child: ListView.builder(
-                  reverse: true,
-                  shrinkWrap: true,
-                  controller: _scrollController,
-                  itemCount: _products.length,
-                  itemBuilder: (context, index) {
-                    return InkWell(
-                        onTap: () {
-                          Navigator.push(context,
-                            MaterialPageRoute(
-                                builder: (context) => ExtendedView(
-                                    id: _products[index].productId,
-                                    title: _products[index].title,
-                                    saving: _products[index].saving,
-                                    category: _products[index].categoryName,
-                                    description: _products[index].description,
-                                    image: _products[index].image,
-                                    price: _products[index].price,
-                                    stream: streamController.stream,
-                                    callback: this)),
-                          );
-                        },
-                        child: ArticleCard(
-                          id: _products[index].productId,
-                          title: _products[index].title,
-                          saving: _products[index].saving,
-                          category: _products[index].categoryName,
-                          description: _products[index].description,
-                          image: _products[index].image,
-                          price: _products[index].price,
-                          callback: this,
-                        ));
-                  }),
-            )),
+                alignment: Alignment.topCenter,
+                child: NotificationListener<ScrollNotification>(
+                  onNotification: (scrollNotification) {
+                    if (scrollNotification is ScrollUpdateNotification) {
+                      _onUpdateScroll();
+                    }
+                    return true;
+                  },
+                  child: ListView.builder(
+                      reverse: true,
+                      shrinkWrap: true,
+                      controller: _scrollController,
+                      itemCount: _products.length,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => ExtendedView(
+                                        id: _products[index].productId,
+                                        title: _products[index].title,
+                                        saving: _products[index].saving,
+                                        category: _products[index].categoryName,
+                                        description:
+                                            _products[index].description,
+                                        image: _products[index].image,
+                                        price: _products[index].price,
+                                        stream: streamController.stream,
+                                        callback: this)),
+                              );
+                            },
+                            child: ArticleCard(
+                              id: _products[index].productId,
+                              title: _products[index].title,
+                              saving: _products[index].saving,
+                              category: _products[index].categoryName,
+                              description: _products[index].description,
+                              image: _products[index].image,
+                              price: _products[index].price,
+                              callback: this,
+                            ));
+                      }),
+                )),
       );
     }
   }
