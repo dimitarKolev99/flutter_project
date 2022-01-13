9e6d1fdb12c032065cae9e48bef96586e5b7db82
Maçoku <rapcehermes@gmail.com>
2022-01-13 12:58:07 +0100

    Add help in help :D

--- lib/view/help.dart
+++ lib/view/help.dart
@@ -29,7 +29,74 @@ class _HelpState extends State<Help> {
     return Scaffold(
       //ScreenUtil.init(context, height:896, width:414, allowFontScaling: true);
         appBar: ExtendedViewAppBar(),
-        body: Text("DOne"),
+        body: ListView(
+          padding: EdgeInsets.all(10),
+          children: <Widget> [
+            Container(
+              padding: EdgeInsets.all(10),
+              color: ThemeChanger.lightBlue,
+              child: Text("1. Produkte zum Merkzettel hinzufügen.\n"
+                  "Sie befinden momentan in LiveFeed oder in der Browserseite oder haben in der Suche ein Produkt gefunden "
+                  "und möchten das Produkt speichern. Klicken Sie dafür das Herzsymbol rechts in der Artikelcard.",
+              style: TextStyle(
+                  color: ThemeChanger.textColor,
+              )),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              color: ThemeChanger.lightBlue,
+              child: Text("2. Produkte vom Merkzettel löschen.\n"
+                  "Klicken Sie das Herzsymbol in dem unteren Bar. Nun sind Sie in der Merkzettelseite gelandet und "
+                  "können die gewünschte Produkte löschen, indem Sie auf das Herzsymbol rechts in der Artikelcard klicken. "
+                  "Wählen Sie in dem Popup-Menü Ja aus und das Produkt wird vom Merkzettel gelöscht.",
+                  style: TextStyle(
+                    color: ThemeChanger.textColor,
+                  )),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              color: ThemeChanger.lightBlue,
+              child: Text("3. Produkte Filtern.\n"
+                  "Klicken Sie das Browsersymbol in dem unteren Bar. Dann wählen Sie eine der Hauptkategorien aus. "
+                  "Nun sind Sie in der Filterseite dieser Hauptkategorie gelandet. "
+                  "Hier können Sie filtern. Am Ende drücken Sie Zeige x Produkte.",
+                  style: TextStyle(
+                    color: ThemeChanger.textColor,
+                  )),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              color: ThemeChanger.lightBlue,
+              child: Text("4. Produkte nach Preise bzw. Rabatte filtern.\n"
+                  "Gehen Sie zur Filterseite und klappen Sie die Preisklasse aus. "
+                  "Hier können Sie die Preisgrenze anpassen und ein Rabatt auswählen.",
+                  style: TextStyle(
+                    color: ThemeChanger.textColor,
+                  )),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              color: ThemeChanger.lightBlue,
+              child: Text("5. Unsere Empfehlung:\n"
+                  "Lassen Sie die Preisklasse immer eingeklappt sein für ein besseres Übersicht der Filterseite. "
+                  "Natürlich nachdem Sie in der Preisklasse fertig sind.",
+                  style: TextStyle(
+                    color: ThemeChanger.textColor,
+                  )),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              color: ThemeChanger.lightBlue,
+              child: Text("3. Produkte Filtern. \n"
+                  "Klicken Sie das Browsersymbol in dem unteren Bar. Dann wählen Sie eine der Hauptkategorien aus."
+                  "Nun sind Sie in der Filterseite dieser Hauptkategorie gelandet. "
+                  "Hier können Sie filtern. Am Ende drücken Sie Zeige x Produkte.",
+                  style: TextStyle(
+                    color: ThemeChanger.textColor,
+                  )),
+            ),
+          ],
+        ),
     );
   }
 }
