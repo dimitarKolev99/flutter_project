f3f3329c783dff2c4c8b97f224cdf2d4dc243f26
siewertsen1999 <73901609+siewertsen1999@users.noreply.github.com>
2022-01-17 13:54:04 +0100

    bugFix
    switch main cats in subcatVIew and everything gets reseted

--- lib/view/subcategory_view.dart
+++ lib/view/subcategory_view.dart
@@ -175,11 +175,13 @@ class _SubcategoryViewState extends State<SubcategoryView> {
                                   return InkWell(
                                       onTap: () {
                                         setState(() {
+                                          //TODO: if Main category changed Producs of old selection still there
                                           widget.callback.numberOfProducts = 0;
                                           Map<String, int> mapReplacement = new Map();
                                           widget.callback.mapOfChosenCategories = mapReplacement;
                                           widget.categoryId = widget
                                               .callback.widget.mainCategoryIds[index];
+                                          widget.callback.bargainsOfChosenCats.clear();
                                           print(
                                               "lasdköjflöaksjf ${widget.callback.widget.mainCategoryIds[index]}");
                                           widget.callback.chosenCategories.clear();
